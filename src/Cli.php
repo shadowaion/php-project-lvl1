8d7578550fd05cd3d266a89eb8903d342150aea8
shadowaion <chiluka_sasha@mail.ru>
2020-12-03 00:28:22 +0300

    Add src/Cli.php to repository

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace php\project\lvl1\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
